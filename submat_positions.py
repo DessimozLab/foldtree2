e8c1361e3eeea779c4799ce8b24751bd81ce97ad
cactuskid <cactuskid13@gmail.com>
2023-11-08 11:22:32 +0100

    submat positions

---
+++ submat_positions.py
@@ -0,0 +1,8 @@
+#use the protein comparison tool to derive equivalent positions
+
+
+# sample the substitutions in the equivalent positions
+
+
+# derive the submat for a given alphabet representation of the structures
+
